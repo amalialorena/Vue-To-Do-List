e4879d5b8e0f8dbce4964aa9924e4175f9438d0a
Amalia <amalialorenaferariu@gmail.com>
2021-11-12 13:35:45 +0100

    init

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+// Rifare l’esercizio della to do list:
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una “x” associata: cliccando su di essa, l’item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+// ATTENZIONE PERO’!!!!
+// Mi comunicano ora dalla regia che lo variamo un attimo,
+// la vostra variante sarà che:
+// il vostro array nei data non avrà delle semplici stringhe, ma sarà un array di oggetti;
+// gli oggetti avranno questo formato:
+// { text: 'Fare la spesa', done: true }, 
+// { text: 'Fare il bucato', done: false }
+// etc. etc..
+// quindi nell’elenco di output ciò che sara “done” sarà barrato, il resto invece con testo normale.
+// Nella richiesta base mi limito al fatto che quel “done” nei task inseriti dall’utente sarà sempre false,
+// gli unici true saranno alcuni nei dati di partenza.
