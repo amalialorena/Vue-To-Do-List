c4fe17c78d952973a6e2083428df482b1a8c9475
Amalia <amalialorenaferariu@gmail.com>
2021-11-12 13:45:43 +0100

    passi logici

--- js/myscript.js
+++ js/myscript.js
@@ -13,3 +13,10 @@
 // quindi nell’elenco di output ciò che sara “done” sarà barrato, il resto invece con testo normale.
 // Nella richiesta base mi limito al fatto che quel “done” nei task inseriti dall’utente sarà sempre false,
 // gli unici true saranno alcuni nei dati di partenza.
+    //creo l'array
+    //stampo in pagina il testo dei miei oggetti con un ciclo
+    //aggiungo l'elemento x in pagina   
+        //con il click andrà a cambiare il valore della proprietà done
+            //e aggiungerà una classe al testo selezionato
+//creo l'input e un bottone
+//pusho il valore dell'input nell'array al click sul bottone
